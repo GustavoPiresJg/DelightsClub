b2809f9f8d0a1ccba1988357089c54bd9e8dcd26
Gustavo Pires <gupires808@gmail.com>
2026-02-08 20:01:57 -0300

    up

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
+// src/app/layout.js
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -8,14 +10,26 @@ const inter = Inter({
 
 export const metadata = {
   title: "Zero Delights Club",
-  description: "Healthy and delicious recipes without sugar",
+  description: "Structured video lessons combined with an exclusive community experience",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <Script id="utmify-pixel" strategy="afterInteractive">
+          {`
+            window.pixelId = "6989106fec899b39d3fd4a45";
+            var a = document.createElement("script");
+            a.setAttribute("async", "");
+            a.setAttribute("defer", "");
+            a.setAttribute("src", "https://cdn.utmify.com.br/scripts/pixel/pixel.js");
+            document.head.appendChild(a);
+          `}
+        </Script>
+      </head>
+
       <body className={inter.className}>{children}</body>
     </html>
   );
 }
- 
